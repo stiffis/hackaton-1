9869a0fda16b92b4a431d8d6cd9482cb20753f29
stiffis <steve.ildefonso@utec.edu.pe>
2025-05-05 22:19:42 -0500

    feat: add project template

---
+++ src/main/java/com/bitWiseComplexity/bigMeow/BigMeowApplication.java
@@ -0,0 +1,13 @@
+package com.bitWiseComplexity.bigMeow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigMeowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BigMeowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bitWiseComplexity/bigMeow/BigMeowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bitWiseComplexity.bigMeow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BigMeowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
